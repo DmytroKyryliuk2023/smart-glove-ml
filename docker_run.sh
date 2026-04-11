c292bb6adcfabb5c971afcb378957749740c9076
DmytroKyryliuk2023 <dmitrokiriluk1208@gmail.com>
2026-04-11 18:29:14 +0300

    Add Docker support with docker-compose and update run script

--- docker_run.sh
+++
@@ -1,2 +0,0 @@
-# docker build -t myapp .
-docker run -p 8000:8000 --memory="4g" myapp
---
+++ start_docker/docker_run.sh
@@ -0,0 +1,2 @@
+# docker build -t dmytrokyryliuk/smart-glove-ml:v1.0 .
+docker run -p 8000:8000 --memory="4g" dmytrokyryliuk/smart-glove-ml:v1.0
